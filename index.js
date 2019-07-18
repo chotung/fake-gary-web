683dbfe33445ba254df843105ba2e6cd5dbdc40c
Charlie Tung <choelestung@gmail.com>
2019-07-18 15:02:05 -0400

    hello

---
+++ index.js
@@ -0,0 +1,22 @@
+// create the array of urls and add them to the 
+// document
+
+const images = {
+  adviceImages : [],
+  projectImages : [
+
+  ]
+}
+
+{/* <img alt="" title="" class="crop-default lazyloaded" data-description="" width="576" height="468"
+      src="//user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,f_auto,h_1440,q_90,w_720/9621/Untitled-1_s4zigm.png"
+      data-src="//user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,f_auto,h_1440,q_90,w_720/9621/Untitled-1_s4zigm.png">
+      <img alt="" title="" class="crop-default lazyloaded" data-description="" width="576" height="468"
+        src="http://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,h_540,w_720/cxiazddwn2es9uwcbw3y.png"
+        data-src="http://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,h_540,w_720/cxiazddwn2es9uwcbw3y.png">
+        <img alt="" title="" class="crop-default lazyloaded" data-description="" width="576" height="468"
+          src="//user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,f_auto,h_1440,q_90,w_720/9621/Untitled-1_copy_zasly7.png"
+          data-src="//user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,f_auto,h_1440,q_90,w_720/9621/Untitled-1_copy_zasly7.png">
+          <img alt="" title="" class="crop-default lazyloaded" data-description="" width="576" height="468"
+            src="http://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,h_540,w_720/ce7xvwaah8ux3vdxlcn2.png"
+            data-src="http://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,h_540,w_720/ce7xvwaah8ux3vdxlcn2.png"> */}
